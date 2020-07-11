efa3533501a9e73f18d1eaf8277fa81d6676194b
takashimakyohei <hagi.suzu@icloud.com>
2020-07-11 10:48:06 +0900

    productsコントローラーのアクション記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,51 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.producer_id = current_producer.id
+    if @product.save
+      redirect_to product_path(@product), notice: "投稿に成功しました"
+    else
+      render :new
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
+    if @product.producer_id != current_producer.id
+      redirect_to products_path, alert: "不正なアクセスです"
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product), notice: "編集に成功しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :content)
   end
 end

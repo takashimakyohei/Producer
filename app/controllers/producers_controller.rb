9372cb128333ed65f0ad1f24aea4867c1cb6ddb7
takashimakyohei <hagi.suzu@icloud.com>
2020-07-11 20:54:44 +0900

    producerコントローラー記述、ユーザー一覧、ユーザー詳細、ユーザー編集画面実装

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -1,10 +1,29 @@
 class ProducersController < ApplicationController
   def index
+    @producers = Producer.all
   end
 
   def show
+    @producer = Producer.find(params[:id])
   end
 
+
   def edit
+    @producer = current_producer
+  end
+
+  def update
+    @producer = current_producer
+    if @producer.update(producer_params)
+      redirect_to producer_path, notice: "ユーザーを更新した"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def producer_params
+    params.require(:producer).permit(:name, :email, :image, :profile, :prefecture)
   end
 end

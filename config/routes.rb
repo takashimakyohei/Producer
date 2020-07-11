252414ed7b79d3832a2586353f0f8fa05bbcd118
takashimakyohei <hagi.suzu@icloud.com>
2020-07-11 11:12:22 +0900

    商品投稿(new)のview記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/edit'
   root to: 'home#index'
+  resources :products
   devise_for :producers
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

f98ff126548c7a3cd6a4522ee73799f7f8a03eea
takashimakyohei <hagi.suzu@icloud.com>
2020-07-11 11:35:32 +0900

    application.html.erbにリンク設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :products
+  resources :producers, only: [:index, :show, :edit, :update]
   devise_for :producers
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

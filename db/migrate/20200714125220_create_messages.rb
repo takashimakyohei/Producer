4d82117ad669344a9b38b61098d2ad12cffe7a8f
takashimakyohei <hagi.suzu@icloud.com>
2020-07-14 21:52:56 +0900

    messageモデル作成、マイグレーション実行

---
+++ db/migrate/20200714125220_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :messages do |t|
+      t.integer :room_id
+      t.boolean :is_user
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end

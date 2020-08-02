57db09c9efe5a9cbb61080465710ffff685d7851
takashimakyohei <hagi.suzu@icloud.com>
2020-08-02 16:44:33 +0900

    設定ファイル修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :branch, "master"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/Producer"
+set :deploy_to, "/var/www/rails/Producer"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
